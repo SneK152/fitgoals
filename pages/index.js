546fe137f8cef39c667f08e0fc959f66ea289879
Raaghaavv <93401518+Raaghaavv@users.noreply.github.com>
2022-07-16 18:10:09 -0700

    ui

--- pages/index.js
+++ pages/index.js
@@ -11,59 +11,133 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
+      <main>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          FitGoals 
         </h1>
 
         <p className={styles.description}>
-          Get started by editing{" "}
-          <code className={styles.code}>pages/index.js</code>
+          Enter in your information down below to get your workout 
+          
         </p>
-
+            
         <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
+            
+              <form>
+                <div className={styles.card}>
+                <h3>Bodyfat</h3>
+                <form >
+                <input required type="number" />
+                </form>
+                </div>
+              
+                <div className={styles.card}>
+                <h3>Age
+                </h3>
+                <form >
+                <input required type="number" />
+                </form>
+                </div>
+            
+                <div className={styles.card}>
+                <h3>Weight</h3>
+                <form >
+                <input required type="number" />
+                </form>
+                </div>
+              
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
+                <div className={styles.card}>
+                <h3>Height </h3>
+                <form >
+                <input required type="number" />
+                </form>
+                </div>
 
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
+                <div className={styles.card}>
+                <h3>Neck </h3>
+                <form >
+                <input required type="number" />
+                </form>
+                </div>
 
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+                <div className={styles.card}>
+                <h3>Chest </h3>
+                <form >
+                <input required type="number" />
+                </form>
+                </div>
+
+                <div className={styles.card}>
+                <h3>Abdomen </h3>
+                <form >
+                <input required type="number" />
+                </form>
+                </div>
+              
+                <div className={styles.card}>
+                <h3>Hip </h3>
+                <form >
+                <input required type="number" />
+                </form>
+                </div>
+                
+                <div className={styles.card}>
+                <h3>Thigh </h3>
+                <form >
+                <input required type="number" />
+                </form>
+                </div>
+
+                <div className={styles.card}>
+                <h3>Knee </h3>
+                <form >
+                <input required type="number" />
+                </form>
+                </div>
+
+                <div className={styles.card}>
+                <h3>Ankle </h3>
+                <form >
+                <input required type="number" />
+                </form>
+                </div>
+
+                <div className={styles.card}>
+                <h3>Biceps </h3>
+                <form >
+                <input required type="number" />
+                </form>
+                </div>
+
+                <div className={styles.card}>
+                <h3>Forearm </h3>
+                <form >
+                <input required type="number" />
+                </form>
+                </div>
+
+                <div className={styles.card}>
+                <h3>Wrist </h3>
+                <form >
+                <input required type="number" />
+                </form>
+                </div>
+
+                <div className={styles.card}>
+                <input type="submit" value="Submit" />
+                </div>
+              </form>
+        </div>
+
+        <div className={styles.centerarrow}>
+          <center>
+          <h3>&rarr;</h3>
+          </center>
         </div>
       </main>
 
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{" "}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
+      
     </div>
   );
 }
